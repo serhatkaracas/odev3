69838a3d6a52131821d4a0d8f911123fefe0bc38
serhatkaracas <121132376+serhatkaracas@users.noreply.github.com>
2023-10-27 22:59:53 +0300

    güncelleme

--- Form1.cs
+++ Form1.cs
@@ -48,7 +48,7 @@ namespace odev3
         }
         private void button4_Click(object sender, EventArgs e)
         {
-            int sonuc = Liste_AdSoyad.Where(ayse => ayse.Contains(textBox4.Text)).Count();
+            int sonuc = Liste_AdSoyad.Where a => a.Contains(textBox4.Text)).Count();
             MessageBox.Show(textBox4.Text +  + sonuc + " adet bulunmaktadýr");
         }
 
